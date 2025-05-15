e6d544d2f7daffb2c30d1ae028452425080a45b8
leocsas <leocristian2014@gmail.com>
2025-05-15 20:32:22 -0300

    Atividade 11

---
+++ exercicios/gerador-abas/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Provider as PaperProvider } from 'react-native-paper';
+import MegaSenaScreen from './screens/MegaSenaScreen';
+import JogoDoBichoScreen from './screens/JogoDoBichoScreen';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
+              if (route.name === 'Mega Sena') iconName = 'numeric';
+              else if (route.name === 'Jogo do Bicho') iconName = 'cow';
+
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'red',
+            tabBarInactiveTintColor: 'gray',
+            headerStyle: { backgroundColor: 'red' },
+            headerTintColor: '#fff',
+          })}
+        >
+          <Tab.Screen name="Mega Sena" component={MegaSenaScreen} />
+          <Tab.Screen name="Jogo do Bicho" component={JogoDoBichoScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
+
